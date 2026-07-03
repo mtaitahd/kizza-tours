7b237311f829c0e83fdf832aa2efbf670b3ce8ce
mtaitahd <mtaitahd@gmail.com>
2026-07-03 22:02:14 +0300

    Fix: move PHPMailer to includes/phpmailer (vendor/ is gitignored) so bookings & inquiries work on live

--- includes/mail.php
+++ includes/mail.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/phpmailer/PHPMailer.php';
-require_once __DIR__ . '/../vendor/phpmailer/SMTP.php';
-require_once __DIR__ . '/../vendor/phpmailer/Exception.php';
+require_once __DIR__ . '/phpmailer/PHPMailer.php';
+require_once __DIR__ . '/phpmailer/SMTP.php';
+require_once __DIR__ . '/phpmailer/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 
